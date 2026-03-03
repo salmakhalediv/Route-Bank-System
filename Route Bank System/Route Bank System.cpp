48c160c3d80b760516b783a7d46d0ce03f640ca1
salma <salma@DESKTOP-5S1E6E2>
2026-03-04 01:59:32 +0200

    testgithub
    Add main function and test comment to entry file
    
    Added a "//github test" comment and implemented the main function, which calls Screens::runApp() to start the application.

--- Route Bank System/Route Bank System.cpp
+++ Route Bank System/Route Bank System.cpp
@@ -6,6 +6,7 @@
 //Admin info to login 1,admin,admin12345,15000
 //Employee info to login 2,salma,salma12345,50000
 //client info to login 2,ibrahim,ibrahim12345,10000
+//github test
 
 int main()
 {
